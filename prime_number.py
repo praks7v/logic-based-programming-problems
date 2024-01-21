c5bc8af2d682a3afe383d47fb7e7f7b2d3834d9b
praks7v <prakashsatvi00@gmail.com>
2024-01-21 11:21:30 +0530

    created a prime number check program

--- prime_number.py
+++ prime_number.py
@@ -1,15 +1,32 @@
+# def is_prime(number):
+#     if number < 2:
+#         return False
+#     for i in range(2, number):
+#         if number % i == 2:
+#             return False
+#     return True
+#
+#
+# if __name__ == "__main__":
+#     input_number = int(input("Enter the number: "))
+#     if is_prime(input_number):
+#         print(f"{input_number} is prime number")
+#     else:
+#         print(f"{input_number} isn't a prime number.")
+
+
 def is_prime(number):
     if number < 2:
         return False
-    for i in range(2, number):
-        if number % i == 2:
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
             return False
     return True
 
 
-if __name__ == "__main__":
-    input_number = int(input("Enter the number: "))
-    if is_prime(input_number):
-        print(f"{input_number} is prime number")
-    else:
-        print(f"{input_number} isn't a prime number.")
+# Example usage:
+user_input = int(input("Enter a number to check if it's prime: "))
+if is_prime(user_input):
+    print(f"{user_input} is a prime number.")
+else:
+    print(f"{user_input} is not a prime number.")
